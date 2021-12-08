71eb87e0cbe3801303040246851144fa18661249
erbinarps <erbinarpaquita1@gmail.com>
2021-12-08 23:36:41 +0700

    Projek Sertifikasi

---
+++ database/migrations/2021_12_07_032546_create_pesanan_details_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePesananDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pesanan_details', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->string('alamat')->nullable();
+            $table->string('nohp')->nullable();
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pesanan_details');
+    }
+}
---
+++ resources/views/index.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <h4><i class="fa fa-user"></i> My Profile</h4>
+                
+        
+            
+</div>
+      
+    </div>
+</div>
+@endsection
